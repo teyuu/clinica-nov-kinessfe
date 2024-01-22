30ccb4fc5bd1d4a9b6dbc4152d5fc37f34271c3c
Matias Tellini <tellini.matias@gmail.com>
2024-01-22 11:58:02 -0300

    fix

--- components/Navbar/MobileMenu.jsx
+++ components/Navbar/MobileMenu.jsx
@@ -5,15 +5,26 @@ import NavItem from './NavItem'
 const MobileMenu = ({ nav, handleNav, navItems }) => {
 
   return (
-    <ul className={`${nav ? "left-0" : "left-[-100%]"} z-[1] fixed top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#619b8a] ease-in-out duration-500 lg:hidden `}>
-    <div className="w-auto py-2 text-4xl font-extrabold text-white m-4">
-      <span className="border-b">KINESSFE</span>
+    <div className={`${nav ? "left-0" : "left-[-100%]"} z-[1] fixed top-0 w-[100%] h-full border-r border-r-gray-900 bg-[#619b8a] ease-in-out duration-500 lg:hidden `}>
+    
+    <div className=' h-[10vh] flex items-center justify-end'>
+    <i className="ri-close-fill text-4xl mr-4  rounded-full border bg-white bg-opacity-20" onClick={handleNav}/>
     </div>
-    {navItems.map((item) => (
-      <NavItem key={item.itemName} {...item} />
-    ))}
+<div className='flex justify-center h-1/2 items-center'>
 
-  </ul>
+    <ul className='w-fit flex flex-col items-center gap-5'>
+    {navItems.map((item) => (
+      <NavItem key={item.itemName} {...item} handleNav={handleNav}/>
+    ))}
+    <button className="bg-[#31544c] uppercase font-bold bg-opacity-80 rounded-xl px-4 py-3 ">
+              PEDí TU TURNO
+            </button>
+    </ul>
+    
+</div>
+
+
+  </div>
   )
 }
 
--- components/Navbar/NavItem.jsx
+++ components/Navbar/NavItem.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import Link from 'next/link'
 
-const NavItem = ({ link, itemName }) => {
+const NavItem = ({ link, itemName, handleNav }) => {
   return (
     <li className={`m-4 uppercase font-semibold hover:border-b-2 duration-100  `}>
-    <Link href={link}>{itemName}</Link>
+    <Link href={link} onClick={handleNav}>{itemName}</Link>
   </li>
   )
 }
