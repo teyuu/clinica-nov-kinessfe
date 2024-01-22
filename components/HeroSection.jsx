f240df5dadfa9e8ffbefc4865f12ac7ede930c0c
Matias Tellini <tellini.matias@gmail.com>
2024-01-22 11:35:15 -0300

    img optimatization

--- components/HeroSection.jsx
+++ components/HeroSection.jsx
@@ -4,6 +4,7 @@ import React from "react";
 import { useEffect, useState } from "react";
 
 import { bgGreen } from "@/data/tailwindClasses";
+import Image from "next/image";
 
 const HeroSection = () => {
   const [loaded, setLoaded] = useState(false);
@@ -21,13 +22,19 @@ const HeroSection = () => {
 
   return (
     <div
-      style={{ backgroundImage: `url(/landing.png)` }}
-      className="bg-cover bg-center h-[750px] lg:h-screen w-full text-white  overflow-hidden"
+      
+      className="relative bg-cover bg-center h-[750px] lg:h-screen w-full text-white  overflow-hidden"
     >
-      <div className="h-full flex flex-col justify-evenly items-center font-PlayfairDisplay px-2">
+
+
+      <Image src='/landing.png' fill alt="landing" quality={100} priority style={{
+          objectFit: 'cover',
+        }}/>
+ 
+      <div className="relative h-full flex flex-col justify-evenly items-center font-PlayfairDisplay px-2">
         <div className= {`${ bgGreen} shadow-xl shadow-gray-400  flex flex-col  justify-center items-center gap-5  bg-opacity-90 w-fit lg:w-[60%] rounded-xl p-10 `}>
           <div
-            className={`${heroTextStyle.title} flex flex-col  `}
+            className={` ${heroTextStyle.title} flex flex-col  `}
           >
             <span className="  ">Rehabilitación</span> <span>y</span>
            <span className="">medicina integral</span> 
@@ -36,22 +43,23 @@ const HeroSection = () => {
             className={`${heroTextStyle.subtitle} ${
               loaded ? "opacity-100 translate-y-0" : "opacity-0 translate-y-10"
             }`}
-          >
+            >
             Cuidado integral para una vida plena: Recupera tu<br/>vitalidad física,
             emocional y mental
           </h2>
         </div>
         <Link
           href="#specializationSection"
-         
-   
-       
-        >
+          
+          
+          
+          >
           <button className={`rounded-full border w-[80px] bg-[#649d8d] bg-opacity-100 py-5`}>
           <i className="ri-arrow-down-line text-2xl"></i>
           </button>
         </Link>
       </div>
+         
     </div>
   );
 };
